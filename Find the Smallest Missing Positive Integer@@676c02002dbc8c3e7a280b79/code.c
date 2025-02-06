64ebc1a36f112d53f6a8d44788c72cb049e93be3
Aryan Singh <aryansingh532006@gmail.com>
2025-02-06 08:51:25 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
-void bubbleSort (int arr[], int n) {
-    for (int i=0; i<n-1; i++){
-        for (int j=0; j<n-i-1; j++){
-            if( arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+
+int findMissingPositive(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        while (arr[i] > 0 && arr[i] <= n && arr[i] != arr[arr[i] - 1]) {
+            int temp = arr[i];
+            arr[i] = arr[temp - 1];
+            arr[temp - 1] = temp;
         }
     }
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != i + 1) {
+            return i + 1;
+        }
+    }
+    return n + 1;
 }
 
 int main() {
-    int n; 
+    int n;
     scanf("%d", &n);
-    int nums[n];
-    for (int i=0; i<n; i++){
-        scanf("%d", &nums[i]);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    bubbleSort(nums,n);
-    for (int i=1; i<n; i++){
-        if(nums[i] != nums[i]-1){
-            printf("%d", nums[i]+1);
-            break;
-        }
-    }
+    int missing = findMissingPositive(arr, n);
+    printf("%d\n", missing);
     return 0;
 }
