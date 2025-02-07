530ef26d451131308bf768cf1cbe4d1324bf38d0
Aryan Singh <aryansingh532006@gmail.com>
2025-02-08 01:30:47 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void sort(char num[], int n){
+void sort(char arr[], int n){
     for (int i=0; i<n-1; i++){
         for (int j=0; j<n-i-1; j++){
             if (arr[j] > arr[j + 1]){
