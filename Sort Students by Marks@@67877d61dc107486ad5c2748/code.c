65fda979c180056d650da43ea4144a5b2760e778
Aryan Singh <aryansingh532006@gmail.com>
2025-02-10 00:13:29 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -31,7 +31,7 @@ int main() {
     bubbleSort(st, n);
     
     for (int i = 0; i < n; i++) {
-        printf("%d %s %.2f\n", st[i].rn, st[i].name, st[i].marks);
+        printf("Roll Number: %d, Name: %s, Marks: %.2f\n", st[i].rn, st[i].name, st[i].marks);
     }
     return 0;
 }
